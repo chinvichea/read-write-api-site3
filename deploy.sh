8e8468bf213edb4cba96d69b721c7bf744af9be0
mbana <m@bana.io>
2019-09-10 14:28:18 +0100

    VuePress build configuration update.

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@
 set -e
 
 # build
-yarn run docs:build
+yarn docs:build
 
 # navigate into the build output directory
 cd docs/.vuepress/dist
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,24 +16,18 @@ module.exports = {
         lastUpdated: 'Last Updated',
         sidebar: [
             {
-                // required
-                title: 'Version 3.1.2 - Group',
-                // optional, which should be a absolute path.
+                title: 'Version 3.1.2',
                 path: '/v3.1.2/',
-                // optional, defaults to true
                 collapsable: false,
-                // optional, defaults to 1
                 sidebarDepth: 1,
                 children: [
                     {
                         title: 'Profiles',
-                        // optional, which should be a absolute path.
                         path: '/v3.1.2/profiles/',
-                        // optional, defaults to true
-                        collapsable: false,
-                        // optional, defaults to 1
+                        collapsable: true,
                         sidebarDepth: 1,
                         children: [
+                            '/v3.1.2/profiles/read-write-data-api-profile',
                             '/v3.1.2/profiles/account-and-transaction-api-profile',
                             '/v3.1.2/profiles/aggregated-polling-api-profile',
                             '/v3.1.2/profiles/callback-url-api-profile',
@@ -42,17 +36,13 @@ module.exports = {
                             '/v3.1.2/profiles/event-notification-subscription-api-profile',
                             '/v3.1.2/profiles/file-payments-api-profile',
                             '/v3.1.2/profiles/payment-initiation-api-profile',
-                            '/v3.1.2/profiles/read-write-data-api-profile',
                             '/v3.1.2/profiles/real-time-event-notification-api-profile',
                         ]
                     },
                     {
                         title: 'Resources and Data Models',
-                        // optional, which should be a absolute path.
                         path: '/v3.1.2/resources-and-data-models/',
-                        // optional, defaults to true
-                        collapsable: false,
-                        // optional, defaults to 1
+                        collapsable: true,
                         sidebarDepth: 1,
                         children: [
                             '/v3.1.2/resources-and-data-models/aisp/',
@@ -63,72 +53,8 @@ module.exports = {
                     },
                     {
                         title: 'References',
-                        // optional, which should be a absolute path.
                         path: '/v3.1.2/references/',
-                        // optional, defaults to true
-                        collapsable: false,
-                        // optional, defaults to 1
-                        sidebarDepth: 1,
-                        children: [
-                            '/v3.1.2/references/domestic-payment-message-formats',
-                            '/v3.1.2/references/namespaced-enumerations',
-                        ]
-                    },
-                ]
-            },
-            {
-                // required
-                title: 'Version 3.1.2 - Group',
-                // // optional, which should be a absolute path.
-                // path: '/v3.1.2/',
-                // optional, defaults to true
-                collapsable: true,
-                // optional, defaults to 1
-                sidebarDepth: 1,
-                children: [
-                    {
-                        title: 'Profiles',
-                        // // optional, which should be a absolute path.
-                        // path: '/v3.1.2/profiles/',
-                        // optional, defaults to true
                         collapsable: true,
-                        // optional, defaults to 1
-                        sidebarDepth: 1,
-                        children: [
-                            '/v3.1.2/profiles/account-and-transaction-api-profile',
-                            '/v3.1.2/profiles/aggregated-polling-api-profile',
-                            '/v3.1.2/profiles/callback-url-api-profile',
-                            '/v3.1.2/profiles/confirmation-of-funds-api-profile',
-                            '/v3.1.2/profiles/event-notification-api-profile',
-                            '/v3.1.2/profiles/event-notification-subscription-api-profile',
-                            '/v3.1.2/profiles/file-payments-api-profile',
-                            '/v3.1.2/profiles/payment-initiation-api-profile',
-                            '/v3.1.2/profiles/read-write-data-api-profile',
-                            '/v3.1.2/profiles/real-time-event-notification-api-profile',
-                        ]
-                    },
-                    {
-                        title: 'Resources and Data Models',
-                        // // optional, which should be a absolute path.
-                        // path: '/v3.1.2/resources-and-data-models/',
-                        // optional, defaults to true
-                        collapsable: true,
-                        // optional, defaults to 1
-                        sidebarDepth: 1,
-                        children: [
-                            '/v3.1.2/resources-and-data-models/aisp/',
-                            '/v3.1.2/resources-and-data-models/pisp/',
-                            '/v3.1.2/resources-and-data-models/cbpii/',
-                            '/v3.1.2/resources-and-data-models/event-notifications/',
-                        ]
-                    },
-                    {
-                        title: 'References',
-                        // // optional, which should be a absolute path.
-                        // path: '/v3.1.2/references/',true
-                        // optional, defaults to true
-                        collapsable: true,
-                        // optional, defaults to 1
                         sidebarDepth: 1,
                         children: [
                             '/v3.1.2/references/domestic-payment-message-formats',
@@ -138,24 +64,54 @@ module.exports = {
                 ]
             },
         ],
-        sidebarDepth: 2,
-        // displayAllHeaders: false,
+        nav: [
+            {
+                text: 'Versions',
+                items: [
+                    {
+                        text: 'Version 3.1.2',
+                        link: '/v3.1.2/',
+                    },
+                ]
+            }
+        ],
+        sidebarDepth: 1,
+        displayAllHeaders: false,
     },
     markdown: {
-        lineNumbers: true,
+        // options for markdown-it-anchor
+        anchor: {
+            permalink: true,
+        },
+        // options for markdown-it-toc
         toc: {
             includeLevel: [
                 1,
                 2,
+                3,
             ],
         },
         extendMarkdown: md => {
             // use more markdown-it plugins!
-            md.set({ breaks: true });
+            // md.set({ breaks: true });
             md.use(require('markdown-it-toc'));
             md.use(require('markdown-it-katex'));
             md.use(require('markdown-it-footnote'));
         },
     },
     cache: false,
+    // These settings control the server that is started when you do `yarn docs:dev`.
+    // It displays useful warnings and errors.
+    //
+    // https://cli.vuejs.org/config/#devserver
+    // https://webpack.js.org/configuration/dev-server/
+    devServer: {
+        clientLogLevel: 'info',
+        compress: true,
+        overlay: {
+            warnings: false,
+            errors: false,
+        },
+        open: 'Google Chrome',
+    },
 };
