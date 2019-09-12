dc83fefb942b7f6c1d56022d0fb01d0fd82207bb
mbana <m@bana.io>
2019-09-12 16:50:11 +0100

    Fix issues with recommended Open Banking fonts

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,293 +3,9 @@
  * Documentation: https://vuepress.vuejs.org/config/.
  */
 
-// https://vuepress.vuejs.org/theme/default-theme-config.html#sidebar
-const create_sidebar = () => {
-    // TODO(mbana): collapsable behaviour turned off for now as it is causing issues. Need to investigate further.
-    const collapsable = true;
-    const sidebarDepth = 0;
-    return [
-        {
-            title: 'Version 3.1.2',
-            path: '/v3.1.2/profiles/read-write-data-api-profile',
-            collapsable: collapsable,
-            sidebarDepth: 1,
-            children: [
-                {
-                    title: 'Account and Transaction API',
-                    path: '/v3.1.2/profiles/account-and-transaction-api-profile',
-                    collapsable: collapsable,
-                    sidebarDepth: 1,
-                    children: [
-                        {
-                            title: 'Account Access Consents',
-                            path: '/v3.1.2/resources-and-data-models/aisp/account-access-consents',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'Accounts',
-                            path: '/v3.1.2/resources-and-data-models/aisp/Accounts',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'Balances',
-                            path: '/v3.1.2/resources-and-data-models/aisp/Balances',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'Transactions',
-                            path: '/v3.1.2/resources-and-data-models/aisp/Transactions',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'Beneficiaries',
-                            path: '/v3.1.2/resources-and-data-models/aisp/Beneficiaries',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'Direct Debits',
-                            path: '/v3.1.2/resources-and-data-models/aisp/direct-debits',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'Products',
-                            path: '/v3.1.2/resources-and-data-models/aisp/Products',
-                            collapsable: collapsable,
-                            sidebarDepth: 1,
-                            children: [
-                                {
-                                    title: 'BCA Product Data Model',
-                                    path: '/v3.1.2/resources-and-data-models/aisp/bca-product-data-model',
-                                    sidebarDepth,
-                                },
-                                {
-                                    title: 'PCA Product Data Model',
-                                    path: '/v3.1.2/resources-and-data-models/aisp/pca-product-data-model',
-                                    sidebarDepth,
-                                },
-                                {
-                                    title: 'Other Product Data Model',
-                                    path: '/v3.1.2/resources-and-data-models/aisp/other-product-data-model',
-                                    sidebarDepth,
-                                },
-                            ],
-                        },
-                        {
-                            title: 'Offers',
-                            path: '/v3.1.2/resources-and-data-models/aisp/Offers',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'Parties',
-                            path: '/v3.1.2/resources-and-data-models/aisp/Parties',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'Scheduled Payments',
-                            path: '/v3.1.2/resources-and-data-models/aisp/scheduled-payments',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'Statements',
-                            path: '/v3.1.2/resources-and-data-models/aisp/Statements',
-                            sidebarDepth,
-                        },
-                    ],
-                },
-                {
-                    title: 'Payment Initiation API ',
-                    path: '/v3.1.2/profiles/payment-initiation-api-profile',
-                    collapsable: collapsable,
-                    sidebarDepth: 1,
-                    children: [
-                        {
-                            title: 'Domestic Payments Consents',
-                            path: '/v3.1.2/resources-and-data-models/pisp/domestic-payment-consents',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'Domestic Payments',
-                            path: '/v3.1.2/resources-and-data-models/pisp/domestic-payments',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'Domestic Scheduled Payment Consents',
-                            path: '/v3.1.2/resources-and-data-models/pisp/domestic-scheduled-payment-consents',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'Domestic Scheduled Payment',
-                            path: '/v3.1.2/resources-and-data-models/pisp/domestic-scheduled-payments',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'Domestic Standing Order Consents',
-                            path: '/v3.1.2/resources-and-data-models/pisp/domestic-standing-order-consents',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'Domestic Standing Orders',
-                            path: '/v3.1.2/resources-and-data-models/pisp/domestic-standing-orders',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'International Payment Consents',
-                            path: '/v3.1.2/resources-and-data-models/pisp/international-payment-consents',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'International Payments',
-                            path: '/v3.1.2/resources-and-data-models/pisp/international-payments',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'International Scheduled Payment Consents',
-                            path: '/v3.1.2/resources-and-data-models/pisp/international-scheduled-payment-consents',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'International Scheduled Payments',
-                            path: '/v3.1.2/resources-and-data-models/pisp/international-scheduled-payments',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'International Standing Order Consents',
-                            path: '/v3.1.2/resources-and-data-models/pisp/international-standing-order-consents',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'International Standing Orders ',
-                            path: '/v3.1.2/resources-and-data-models/pisp/international-standing-orders',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'File Payment Consents',
-                            path: '/v3.1.2/resources-and-data-models/pisp/file-payment-consents',
-                            sidebarDepth,
-                        },
-                        {
-                            title: 'File Payments',
-                            path: '/v3.1.2/resources-and-data-models/pisp/file-payments',
-                            sidebarDepth,
-                        },
-                    ],
-                },
-                {
-                    title: 'Confirmation of Funds API Profile',
-                    path: '/v3.1.2/profiles/confirmation-of-funds-api-profile',
-                    collapsable: collapsable,
-                    sidebarDepth: 1,
-                    children: [
-                        {
-                            title: 'Funds Confirmation Consent',
-                            path: '/v3.1.2/resources-and-data-models/cbpii/funds-confirmation-consent',
-                        },
-                        {
-                            title: 'Funds Confirmation',
-                            path: '/v3.1.2/resources-and-data-models/cbpii/funds-confirmation',
-                        },
-                    ],
-                },
-                {
-                    title: 'Event Notification API',
-                    path: '/v3.1.2/profiles/event-notification-api-profile',
-                    collapsable: collapsable,
-                    sidebarDepth: 1,
-                    children: [
-                        {
-                            title: 'Event Notification Subscription API',
-                            path: '/v3.1.2/profiles/event-notification-subscription-api-profile',
-                        },
-                        {
-                            title: 'Callback URL API',
-                            path: '/v3.1.2/profiles/callback-url-api-profile',
-                        },
-                        {
-                            title: 'Real Time Event Notification API',
-                            path: '/v3.1.2/profiles/real-time-event-notification-api-profile',
-                        },
-                        {
-                            title: 'Aggregated Polling API',
-                            path: '/v3.1.2/profiles/aggregated-polling-api-profile',
-                        },
-                    ],
-                },
-                // {
-                //   title: 'Profiles',
-                //   path: '/v3.1.2/profiles/',
-                //   collapsable: collapsable,
-                //   sidebarDepth: 1,
-                //   children: [
-                //     '/v3.1.2/profiles/aggregated-polling-api-profile',
-                //     '/v3.1.2/profiles/callback-url-api-profile',
-                //     '/v3.1.2/profiles/event-notification-subscription-api-profile',
-                //     '/v3.1.2/profiles/file-payments-api-profile',
-                //     '/v3.1.2/profiles/real-time-event-notification-api-profile',
-                //   ]
-                // },
-                {
-                    title: 'Resources and Data Models',
-                    path: '/v3.1.2/resources-and-data-models/',
-                    collapsable: collapsable,
-                    sidebarDepth: 1,
-                    children: [
-                        '/v3.1.2/resources-and-data-models/aisp/',
-                        '/v3.1.2/resources-and-data-models/pisp/',
-                        '/v3.1.2/resources-and-data-models/cbpii/',
-                        '/v3.1.2/resources-and-data-models/event-notifications/',
-                    ]
-                },
-                {
-                    title: 'References',
-                    path: '/v3.1.2/references/',
-                    collapsable: collapsable,
-                    sidebarDepth: 1,
-                    children: [
-                        '/v3.1.2/references/domestic-payment-message-formats',
-                        '/v3.1.2/references/namespaced-enumerations',
-                    ]
-                },
-            ],
-        },
-    ];
-}
-
-const create_nav = () => {
-    return false;
-
-    // We currently don't want an nav at the top.
-    // return [
-    //   {
-    //     text: 'Versions',
-    //     items: [
-    //       {
-    //         text: 'Version 3.1.2',
-    //         link: '/v3.1.2/',
-    //       },
-    //     ]
-    //   }
-    // ]
-};
-
-const create_dev_server = () => {
-    // https://cli.vuejs.org/config/#devserver
-    // https://webpack.js.org/configuration/dev-server/
-    return {
-        // https://webpack.js.org/configuration/dev-server/#devserverclientloglevel
-        clientLogLevel: 'info',
-        // https://webpack.js.org/configuration/dev-server/#devservercompress
-        compress: true,
-        // https://webpack.js.org/configuration/dev-server/#devserveroverlay
-        overlay: {
-            warnings: false,
-            errors: false,
-        },
-        // https://webpack.js.org/configuration/dev-server/#devserveropen
-        open: 'Google Chrome',
-        // https://webpack.js.org/configuration/dev-server/#devserverwatchcontentbase
-        watchContentBase: true,
-    };
-}
+const sidebar = require('./sidebar.js');
+const nav = require('./nav.js');
+const devServer = require('./devServer.js');
 
 module.exports = {
     base: '/read-write-api-site2/',
@@ -297,8 +13,8 @@ module.exports = {
     description: 'Read/Write API Standards',
     themeConfig: {
         lastUpdated: 'Last Updated',
-        sidebar: create_sidebar(),
-        nav: create_nav(),
+        sidebar,
+        nav,
         sidebarDepth: 1,
         // https://vuepress.vuejs.org/theme/default-theme-config.html#displaying-header-links-of-all-pages
         displayAllHeaders: false,
@@ -358,7 +74,7 @@ module.exports = {
     // These settings configures the server that is started when you do `yarn docs:dev`.
     // With the current configuration settings, if you open Google Chrome DevTools after navigating
     // to the site, it will display any compilation warnings/errors related to Markdown amongst other things.
-    devServer: create_dev_server(),
+    devServer,
     // https://cli.vuejs.org/config/#css-sourcemap
     css: {
         sourceMap: true,
@@ -387,4 +103,9 @@ module.exports = {
     // This will disable ES5 transpilation and polyfills for IE, and result in faster builds and smaller files.
     // Set to true, if we want to support IE.
     evergreen: true,
+    // TODO(mbana): WIP.
+    // extraWatchFiles: [
+    //     require.resolve('./sidebar.js'),
+    //     // or '.vuepress/sidebar.js'
+    // ],
 };
---
+++ docs/.vuepress/nav.js
@@ -0,0 +1,16 @@
+// https://vuepress.vuejs.org/theme/default-theme-config.html#navbar
+
+module.exports = false;
+
+// We currently don't want an nav at the top.
+// module.exports = [
+//   {
+//     text: 'Versions',
+//     items: [
+//       {
+//         text: 'Version 3.1.2',
+//         link: '/v3.1.2/',
+//       },
+//     ]
+//   }
+// ]
